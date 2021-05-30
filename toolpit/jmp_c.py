d663cfa01fd3c14551fcc8c3b3f6d65b9db77916
musangtara <musangtara@gmail.com>
2021-05-30 11:29:00 +0700

    Initial commit

--- toolpit/jmp_c.py
+++ toolpit/jmp_c.py
@@ -2,9 +2,10 @@ import os, sys
 from typing import List
 import threading
 
-f = open("jmpfile.py", "r").read()
+f = open("commandlist.txt", "r").read()
 print(f)
 command = f.splitlines()
+
 def run_command():
 	for i in range(len(command)):
 		threading.Thread(target=os.system(f"{command[i]}"))
